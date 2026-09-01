eeed98e87a91dfc0c700de7fee1e85b8208ebb42
Otiosun <natanaelgames19@gmail.com>
2026-09-01 11:12:40 -0300

    style: format projection contract

--- packages/map-renderer/src/projection.ts
+++ packages/map-renderer/src/projection.ts
@@ -67,11 +67,7 @@ export interface ProjectionAnnotation extends ProjectionItemBase<'annotation'> {
 }
 
 export type MapProjectionItem =
-  | ProjectionNode
-  | ProjectionRoute
-  | ProjectionArea
-  | ProjectionRing
-  | ProjectionAnnotation;
+  ProjectionNode | ProjectionRoute | ProjectionArea | ProjectionRing | ProjectionAnnotation;
 
 export interface MapProjection {
   projectionVersion: 1;
